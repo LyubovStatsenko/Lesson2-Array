16989a302558b45ec85b903a24beb83dc4cb17b2
Lyubov Statsenko <stlarek10@gmail.com>
2024-02-02 14:08:12 +0600

    add Task 2 - 3

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,13 +1,32 @@
 ﻿int n = 5;
-int [] array = {2,4,5,8,7}; 
+int[] array = { 2, 4, 5, 8, 7 };
 int i = 0;
-int max = array[0]; 
- while(i<n)
+int max = array[0];
+while (i < n)
 {
     if (array[i] > max)
     {
         max = array[i];
     }
-    i = i + 1; 
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{
+    if (e > max) 
+    {
+        max = e;
+    }
 }
 Console.WriteLine(max);
